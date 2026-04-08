d0f9060ee7744727e39c22da2de61804d5bc5195
Maximos Frountzos <148377715+iMaxinq@users.noreply.github.com>
2026-04-08 20:28:27 +0300

    Delete scheduler directory

--- scheduler/Makefile
+++
@@ -1,9 +0,0 @@
-CC = gcc
-CFLAGS = -Wall
-LDFLAGS = -lm
-
-all: scheduler scheduler_io
-
-
-clean:
-	rm -f scheduler scheduler_io
--- scheduler/run.sh
+++
@@ -1,9 +0,0 @@
-# ./scheduler FCFS homogeneous.txt
-# ./scheduler RR 1000 homogeneous.txt
-
-# ./scheduler FCFS reverse.txt
-# ./scheduler RR 1000 reverse.txt
-
-# ./scheduler_io FCFS mixed.txt
-# ./scheduler_io RR 1000 mixed.txt
-
